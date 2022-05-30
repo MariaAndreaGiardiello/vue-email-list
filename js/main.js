e8602f71b2b7c089c30d763c9fc08a0a163cdfe5
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-05-30 16:20:32 +0200

    aggiunto ciclo funzionante per generazione 10 email

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@
 //generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 //Bonus
 //Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        randomEmail: [],
+    },
+    created () {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((email) => {
+                this.randomEmail[i] = email.data.response; 
+                this.randomEmail.push(email.response);
+            })
+        }
+    }
+})
